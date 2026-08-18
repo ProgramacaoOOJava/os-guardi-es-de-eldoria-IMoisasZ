efda9f6e07b5f01072fa656c94c2e5245a18dfd0
Moisés Barbosa dos Santos <68626845+IMoisasZ@users.noreply.github.com>
2026-08-18 02:07:27 +0000

    Criado o desafio no nivel novato

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
+package nivel_novato.desafio;
+
+// Classe Main para testar a classe Personagem
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        // Istanciando dois objetos da classe Personagem
+        Personagem personagem1 = new Personagem("Arthemis", "Mago", 10, 100, 50.0);
+        Personagem personagem2 = new Personagem("Thorin", "Guerreiro", 15, 150, 70.0);
 
-        .
-        .
-        .
-
+        // Exibindo o status dos personagens
+        personagem1.exibirStatus();
+        personagem2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,3 +1,4 @@
+// Criando a classe Personagem com atributos e métodos
 public class Personagem {
     String nome;
     String classe;
@@ -5,20 +6,19 @@ public class Personagem {
     int pontosDeVida;
     double poderBase;
 
+    // Criado o construtor da classe Personagem
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
+    // Criado o método exibirStatus para mostrar as informações do personagem
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome + "\nClasse: " + classe + "\nNível: " + nivel + "\nPontos de Vida: "
+                + pontosDeVida + "\nPoder Base: " + poderBase + "\n-------------------------");
     }
 }
-
